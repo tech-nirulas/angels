8b8b807dd98475f9d2079fd675cdeeaab84e5ba8
Ujjwal Sharma <ujjwalsharma11290@gmail.com>
2026-05-05 11:34:06 +0530

    Add TypeScript configuration to ignore build errors in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
     remotePatterns: [
       {
